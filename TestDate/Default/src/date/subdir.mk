be0fe5536bc7dc0ccad3d917a2053009ad1b1a36
Profe Juan <profejuan@criteria.com>
2018-02-23 08:59:07 +0100

    Makefiles added for default run

---
+++ TestDate/Default/src/date/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/date/Date.cpp \
+../src/date/DateTime.cpp \
+../src/date/Time.cpp 
+
+OBJS += \
+./src/date/Date.o \
+./src/date/DateTime.o \
+./src/date/Time.o 
+
+CPP_DEPS += \
+./src/date/Date.d \
+./src/date/DateTime.d \
+./src/date/Time.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/date/%.o: ../src/date/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
