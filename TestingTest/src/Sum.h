3b91660b5878c611b45ecc5234aa9ea46f6ca2ad
Juan <juan@ubuntu.com>
2018-03-04 14:19:39 +0100

    Exceptions test added to testing example. SetUp and TearDown methods added.

--- TestingTest/src/Sum.h
+++ TestingTest/src/Sum.h
@@ -1,6 +1,14 @@
+/*
+ * Adds numbers between min and max
+ * */
 class Sum {
+	int min, max;
 public:
+	Sum() : min(0), max(10) {}
+	Sum(int min, int max) : min(min), max(max) {}
 	int sum(int a, int b) {
+		if (a < 0 || a > 10) throw "a is out of range";
+		if (b < 0 || b > 10) throw "b is out of range";
 		return a + b;
 	}
 };
--- TestingTest/tests/SumTest.cpp
+++ TestingTest/tests/SumTest.cpp
@@ -2,10 +2,23 @@
 #include "gtest/gtest.h"
 
 class SumTest: public ::testing::Test {
-protected:
+protected: // Puede ser public o protected, no private
 	Sum sum1;
+	virtual void SetUp() { // Función opcional para inicializar el test
+		sum1 = Sum(1, 8);
+	}
+	virtual void TearDown() {
+		// Función opcional de fin de test
+	}
 };
 
+// Cada test tiene una instancia distinta de sum1
 TEST_F(SumTest, addingCorrectly) {
 	ASSERT_EQ(sum1.sum(2, 2), 4);
+}
+
+TEST_F(SumTest, inRange) {
+	EXPECT_ANY_THROW(sum1.sum(-3, 2));
+	EXPECT_ANY_THROW(sum1.sum(1, 12));
+	EXPECT_NO_THROW(sum1.sum(1, 8));
 }
