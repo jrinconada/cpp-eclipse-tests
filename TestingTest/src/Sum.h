bd991ae665c7935a9bd43b0500918e6bd0af2abe
Profe Juan <profejuan@criteria.com>
2018-03-19 08:11:16 +0100

    Testing example made more dynamic

--- TestingTest/src/Sum.h
+++ TestingTest/src/Sum.h
@@ -7,8 +7,8 @@ public:
 	Sum() : min(0), max(10) {}
 	Sum(int min, int max) : min(min), max(max) {}
 	int sum(int a, int b) {
-		if (a < 0 || a > 10) throw "a is out of range";
-		if (b < 0 || b > 10) throw "b is out of range";
+		if (a < min || a > max) throw "a is out of range";
+		if (b < min || b > max) throw "b is out of range";
 		return a + b;
 	}
 };
